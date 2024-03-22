4e24f77f6d87213901b9c18cec39dbfe3106f68f
Rayssa <rayssafreitas13@gmail.com>
2024-03-22 15:59:24 -0300

    Configurando aplicação heroku

---
+++ src/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment =
+{
+    production: false,
+    baseUrl: 'https://frozen-mesa-26923-789bade3fecb.herokuapp.com'
+}
--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment =
 {
     production: false,
-    //endPoint: 'http://localhost:8081'
-    apiUrl: 'https://frozen-mesa-26923-789bade3fecb.herokuapp.com'
+    baseUrl: 'http://localhost:8081'
+
 }
