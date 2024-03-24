1647064d65f87a71635f9dbceedbbac30ac45930
Rayssa <rayssafreitas13@gmail.com>
2024-03-24 14:11:58 -0300

    Corrige environment.prod.td

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment =
 {
     production: true,
-    //apiUrl: 'https://frozen-mesa-26923-789bade3fecb.herokuapp.com'
-    apiUrl: 'http://localhost:8081'
+    apiUrl: 'https://frozen-mesa-26923-789bade3fecb.herokuapp.com'
+    //apiUrl: 'http://localhost:8081'
 }
